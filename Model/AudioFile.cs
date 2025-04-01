b71d787313054c994d186f1ac5d8b897d3a87648
mansurHafizPhila <118294685+mansurHafizPhila@users.noreply.github.com>
2025-04-01 14:35:13 -0400

    Initial commit

---
+++ Model/AudioFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArchiveHist.Model
+{
+    public partial class AudioFile
+    {
+        [Key]
+        [Column("A_id")]
+        public int AId { get; set; }
+        [Column("Link Name")]
+        [Unicode(false)]
+        public string? LinkName { get; set; }
+        [Column("C_id")]
+        public int? CId { get; set; }
+
+        [ForeignKey("CId")]
+        [InverseProperty("AudioFiles")]
+        public virtual Collection? CIdNavigation { get; set; }
+    }
+}
---
+++ Model/Photo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArchiveHist.Model
+{
+    [Table("Photo")]
+    public partial class Photo
+    {
+        [Key]
+        [Column("P_id")]
+        public int PId { get; set; }
+        [Unicode(false)]
+        public string? Title { get; set; }
+        [Column("Artist/Agency")]
+        [StringLength(50)]
+        [Unicode(false)]
+        public string? ArtistAgency { get; set; }
+        [StringLength(50)]
+        [Unicode(false)]
+        public string? Year { get; set; }
+        [Unicode(false)]
+        public string? Link { get; set; }
+        [Unicode(false)]
+        public string? Notes { get; set; }
+        [Column("C_id")]
+        public int? CId { get; set; }
+
+        [ForeignKey("CId")]
+        [InverseProperty("Photos")]
+        public virtual Collection? CIdNavigation { get; set; }
+    }
+}
